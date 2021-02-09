4a6a447a88bd1812e65b51050379a1b4d6fa57c8
HenryZeng <zengdeveloper@qq.com>
2021-02-09 08:02:18 +0800

    修改指针设置函数，明确指针操作范围，减少指针传来传去的问题

--- Classes/Support/Controls/Controller.cpp
+++ Classes/Support/Controls/Controller.cpp
@@ -1,17 +1,20 @@
 #include "Controller.h"
 
+
 // 指针设定
 
-void ControlBox::setMainWindow(GLFWwindow *Window){
-	_MainWindow = Window;
+void ControlBox::setMainWindow(GLFWwindow *_Window_){
+	_MainWindow = _Window_;
 }
 
-void ControlBox::setMouseEvent(EventListenerMouse *Mouse){
-	_MouseEvents = Mouse;
+void ControlBox::setDragonBones(dragonBones::CCFactory *_Db_factory_,dragonBones::CCArmatureDisplay *_Db_armatureDisplay_){
+	_Db_factory = _Db_factory_;
+	_Db_armatureDisplay = _Db_armatureDisplay_;
 }
+
 
 // 参数设定
 
-void ControlBox::setWindowsOnTop(bool key){
-	glfwSetWindowAttrib(_MainWindow, GLFW_FLOATING, key);
+void ControlBox::setWindowsOnTop(bool _key_){
+	glfwSetWindowAttrib(_MainWindow, GLFW_FLOATING, _key_);
 }

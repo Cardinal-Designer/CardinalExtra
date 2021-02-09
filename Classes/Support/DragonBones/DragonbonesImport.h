e4976327b93487b4a6e1070f470e48738ae56ee1
HenryZeng <zengdeveloper@qq.com>
2021-02-09 08:08:09 +0800

    + 修改初始化Scene返回函数 + 增加外部初始化的功能，让人物初始化在创建之后初始化 + 增设Dragonbones必要创建参数的共享变量

--- Classes/Support/DragonBones/DragonbonesImport.h
+++ Classes/Support/DragonBones/DragonbonesImport.h
@@ -1,3 +1,6 @@
+#ifndef DRAGON_BONES_IMPORT_
+#define DRAGON_BONES_IMPORT_
+
 #include "../Base/CardinalScene.h"
 #include "../Controls/Controller.h"
 #include "dragonBones/cocos2dx/CCDragonBonesHeaders.h"
@@ -5,17 +8,39 @@
 class DragonBonesImport : Cardinal_draw
 {
 public:
+
     CREATE_FUNC(DragonBonesImport);
-    static ControlBox *Control; // ControlBox 指针
-    static cocos2d::Scene *createScene()
-    {
+
+    static cocos2d::Scene *CreateScene(ControlBox *Control_,std::string DragonBonesData_,std::string TextureAtlasData_,std::string armatureName_){
         auto scene = cocos2d::Scene::create();
         auto layer = DragonBonesImport::create();
+
+        layer->Control = Control_;
+        layer->Init_Layer(DragonBonesData_,TextureAtlasData_,armatureName_);
 
         scene->addChild(layer);
         return scene;
     }
 
+    ControlBox *Control; // ControlBox 指针
+
+    void Init_Layer(std::string DragonBonesData_,std::string TextureAtlasData_,std::string armatureName_);
+
+    dragonBones::CCFactory *factory = dragonBones::CCFactory::getFactory();
+    dragonBones::CCArmatureDisplay *armatureDisplay;
+
+    std::string DragonBonesData; // 龙骨数据路径 json
+    std::string TextureAtlasData; // 纹理数据路径 json
+    std::string armatureName; // 骨架名称
+
+    virtual void SceneInit();
 protected:
-    virtual void _onStart();
+    
+
+    void Mouse_onMouseUp(cocos2d::Event *event);
+    void Mouse_onMouseMove(cocos2d::Event *event);
+
+    void Db_init();
 };
+
+#endif

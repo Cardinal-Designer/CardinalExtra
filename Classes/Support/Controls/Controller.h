36cd87819d8966b547f34c395d373fa91ce7cbf2
HenryZeng <zengdeveloper@qq.com>
2021-02-09 08:03:33 +0800

    增加部分鼠标事件，增加对绘图引擎的跨编译单元调用

--- Classes/Support/Controls/Controller.h
+++ Classes/Support/Controls/Controller.h
@@ -3,21 +3,36 @@
 
 #include "cocos2d.h"
 #include "dragonBones/cocos2dx/CCDragonBonesHeaders.h"
+
 using cocos2d::EventListenerMouse;
+
+#define _
+
 
 class ControlBox
 {
 protected:
     GLFWwindow *_MainWindow;
     EventListenerMouse *_MouseEvents;
-    dragonBones::CCFactory * dbFactory;
-    int key;
-public:
-    void setMainWindow(GLFWwindow *Window);
-    void setMouseEvent(EventListenerMouse *Mouse);
 
-    void setWindowsOnTop(bool key);
+    dragonBones::CCFactory *_Db_factory;
+    dragonBones::CCArmatureDisplay *_Db_armatureDisplay;
+
+    int _Mode;
+public:
+    void setMainWindow(GLFWwindow *_Window_);
+    void setWindowsOnTop(bool _key_);
+    void setRuntimeMode();
+    void setDragonBones(dragonBones::CCFactory *_Db_factory_,dragonBones::CCArmatureDisplay *_Db_armatureDisplay_);
+
+    // Mouse Event
+    void Mouse_onMouseUp(cocos2d::EventMouse *event);
+    void Mouse_onMouseMove(cocos2d::EventMouse *event);
+    // Mouse Event
 
 };
+
+namespace MouseData{};
+namespace Link{};
 
 #endif

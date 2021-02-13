2272668d09bef6e7a563994f2f645b43b9fa3cbb
HenryZeng <zengdeveloper@qq.com>
2021-02-13 14:17:40 +0800

    + 增加设置解析引擎类型的函数 setMode + 增加文件搜索路径设置函数 setSearchPath + 增加ControlBox初始化函数

--- Classes/Support/Controls/Controller.cpp
+++ Classes/Support/Controls/Controller.cpp
@@ -1,20 +1,70 @@
 #include "Controller.h"
 
+using cocos2d::ControlBox;
+
+ControlBox *ControlBox::controlbox = nullptr;
+namespace cocos2d {
+    namespace Mode {
+        // 解析引擎模式编号对应
+        const int DragonBones = 0;
+        const int Spine = 1;
+        const int Live2D = 2;
+    }
+}
+
+void ControlBox::StaticInit(){
+    // ControlBox 初始化
+    setScale(scale_);
+    CCLOG("StaticInit Finish");
+}
+
 
 // 指针设定
 
-void ControlBox::setMainWindow(GLFWwindow *_Window_){
-	_MainWindow = _Window_;
+
+void ControlBox::setMainWindow(GLFWwindow *Window_) {
+    // 设置GLFW窗口对象指针
+    MainWindow_ = Window_;
 }
 
-void ControlBox::setDragonBones(dragonBones::CCFactory *_Db_factory_,dragonBones::CCArmatureDisplay *_Db_armatureDisplay_){
-	_Db_factory = _Db_factory_;
-	_Db_armatureDisplay = _Db_armatureDisplay_;
+void ControlBox::setDragonBones(dragonBones::CCFactory *Db_f, dragonBones::CCArmatureDisplay *Db_a) {
+    // 记录龙骨的工厂模式和动画显示对象的指针
+    dragonbones_.armatureDisplay = Db_a;
+    dragonbones_.factory = Db_f;
 }
 
+void ControlBox::setMode(int m_) {
+    // 设置解析引擎的模式
+    mode_ = m_;
+}
+
+void ControlBox::setSearchPath(std::string path) {
+    // 设置搜索路径
+    searchPath_ = std::move(path);
+}
+
+void ControlBox::setDragonBones_info(dragonbones_info dbi) {
+    // 设置龙骨模型文件信息
+    dragonbones_info_ = std::move(dbi);
+}
 
 // 参数设定
 
-void ControlBox::setWindowsOnTop(bool _key_){
-	glfwSetWindowAttrib(_MainWindow, GLFW_FLOATING, _key_);
+void ControlBox::setWindowsOnTop(bool key_) {
+    // 设置窗口是否置顶
+    glfwSetWindowAttrib(MainWindow_, GLFW_FLOATING, key_);
+}
+
+void ControlBox::setScale(float scale) {
+    scale_ = scale;
+
+    if(!dragonbones_.armatureDisplay){ // 防止不当初始化
+        return;
+    }
+
+    switch (mode_) { // 根据引擎设置缩放系数
+        case cocos2d::Mode::DragonBones:
+            dragonbones_.armatureDisplay->setScale(scale_);
+            break;
+    }
 }

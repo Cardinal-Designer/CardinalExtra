0b78ffd46b0b91e6e2c9568402f5faf9af2bc274
HenryZeng <zengdeveloper@qq.com>
2021-02-13 14:19:13 +0800

    + 增加设置解析引擎类型的函数 setMode + 增加文件搜索路径设置函数 setSearchPath + 增加ControlBox初始化函数 + 整理对龙骨的对象的引入方式 + 把龙骨对象创建前后状态隔离为两个结构体 dragonbones_info dragonbones_obj + 自由实例改为单例创建/获取 + 增加鼠标事件

--- Classes/Support/Controls/Controller.h
+++ Classes/Support/Controls/Controller.h
@@ -6,33 +6,75 @@
 
 using cocos2d::EventListenerMouse;
 
-#define _
+namespace cocos2d {
+    struct dragonbones_info {
+        std::string DragonBonesData; // 龙骨数据路径 json
+        std::string TextureAtlasData; // 纹理数据路径 json
+        std::string armatureName; // 骨架名称
+    };
 
+    struct dragonbones_obj {
+        dragonBones::CCFactory *factory = nullptr; // 龙骨的工厂模式对应指针
+        dragonBones::CCArmatureDisplay *armatureDisplay = nullptr; // 龙骨的动画显示对象指针
+    };
+}
 
-class ControlBox
-{
-protected:
-    GLFWwindow *_MainWindow;
-    EventListenerMouse *_MouseEvents;
+namespace cocos2d {
+    class ControlBox {
+    protected:
+        GLFWwindow *MainWindow_; // GLFW窗口对象
+        dragonbones_obj dragonbones_; // 龙骨对象指针集合
+        dragonbones_info dragonbones_info_; // 龙骨模型文件信息集合
+        int mode_; // 解释引擎的模式
+        std::string searchPath_; // 文件搜索路径
+        float scale_ = 1.0; // 缩放系数
 
-    dragonBones::CCFactory *_Db_factory;
-    dragonBones::CCArmatureDisplay *_Db_armatureDisplay;
+    public:
+        static ControlBox *controlbox; // ControlBox 单例
 
-    int _Mode;
-public:
-    void setMainWindow(GLFWwindow *_Window_);
-    void setWindowsOnTop(bool _key_);
-    void setRuntimeMode();
-    void setDragonBones(dragonBones::CCFactory *_Db_factory_,dragonBones::CCArmatureDisplay *_Db_armatureDisplay_);
+        static ControlBox *getInstance() { // 获取 或 创建 ControlBox 单例
+            if (!controlbox) {
+                controlbox = new(std::nothrow) ControlBox;
+            }
 
-    // Mouse Event
-    void Mouse_onMouseUp(cocos2d::EventMouse *event);
-    void Mouse_onMouseMove(cocos2d::EventMouse *event);
-    // Mouse Event
+            return controlbox;
+        }
+        void StaticInit();
 
-};
+        void setMode(int m_);
 
-namespace MouseData{};
-namespace Link{};
+        void setSearchPath(std::string path);
+
+        void setMainWindow(GLFWwindow *Window_);
+
+        void setDragonBones_info(dragonbones_info dbi);
+
+        void setDragonBones(dragonBones::CCFactory *Db_factory_, dragonBones::CCArmatureDisplay *Db_armatureDisplay_);
+
+        // Api
+        void setWindowsOnTop(bool key_);
+
+        void setScale(float scale);
+        // Api
+
+        // Mouse Event
+        void Mouse_onMouseUp(cocos2d::EventMouse *event);
+
+        void Mouse_onMouseMove(cocos2d::EventMouse *event);
+
+        void Mouse_onMouseDown(cocos2d::EventMouse *event);
+        // Mouse Event
+
+        std::string getSearchPath();
+
+        dragonbones_info getDragonbonesInfo();
+
+    };
+
+    namespace MouseData {};
+    namespace Link {};
+
+    namespace Mode{}
+}
 
 #endif
